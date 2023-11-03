6f383ceafd70258c468f991982a9a52a9462884d
fehaubert <fernandahaubert@icloud.com>
2023-11-03 18:05:34 -0300

    comandosDocumentados

--- src/app.js
+++ src/app.js
@@ -5,12 +5,16 @@ const app = express();
 
 // Importar as rotas para serem executadas na aplicação
 const crudRouter = require('./routes/crudRouter');
+// Importar as rotas para serem executadas na aplicação
+const alunosRouter = require('./routes/alunosRouter');
 
 // Importar o pacote dotenv
 const dotenv = require('dotenv').config();
 
 // Habilitar a utilização do crudRouter
 app.use('/api', crudRouter);
+// Habilitar a utilização do alunosRouter
+app.use('/api', alunosRouter);
 
 // Setar a porta do servidor, a partir do arquivo .env
 app.set('port', process.env.PORT);
